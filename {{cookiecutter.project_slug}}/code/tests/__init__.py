bd68a993feba8f218b4e93385726f951acb14e6c
simon.hofmann <shoffmann@cbs.mpg.de>
2023-11-06 10:48:23 +0100

    add pytest info

--- {{cookiecutter.project_slug}}/code/tests/__init__.py
+++ {{cookiecutter.project_slug}}/code/tests/__init__.py
@@ -1,1 +1,8 @@
-"""Unit test package for {{ cookiecutter.project_slug }}."""
+"""
+Unit test package for {{ cookiecutter.project_slug }}
+{%- if cookiecutter.use_pytest == 'y' %}
+    pytest                  # (standard run through the tests)
+        --cov               # (check how much (%) of the code is tested already)
+        --cov-report=html   # (to explore which parts of the code are not tested yet).
+{%- endif -%}
+"""
