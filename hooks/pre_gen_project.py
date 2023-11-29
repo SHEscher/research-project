0e5d86aadfb15c6d1ec1476db86bdf737702b4d5
simon.m.hofmann <simon.hofmann@cbs.mpg.de>
2023-11-29 01:28:41 +0100

    add check if conda is installed

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,7 +3,9 @@
 
 # %% Import
 import re
+import subprocess
 import sys
+
 
 
 # %% Set global vars & paths >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
@@ -12,9 +14,26 @@ MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 module_name = '{{ cookiecutter.project_slug }}'
 
 
+# %% Functions >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
+
+def is_conda_installed():
+    try:
+        subprocess.check_output(['conda', '--version'])
+        return True
+    except Exception:
+        return False
+
+
 # %% __main__  >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o
 
 if __name__ == '__main__':
+
+    {%- if cookiecutter.create_conda_env == 'y' -%}
+    # In case of True add your content here
+    if not is_conda_installed():
+        print("\033[31m\nERROR: Conda is not installed on your machine. Please install and run again!\033[0m\n")
+        sys.exit(1)
+    {% endif %}
 
     if not re.match(MODULE_REGEX, module_name):
 
