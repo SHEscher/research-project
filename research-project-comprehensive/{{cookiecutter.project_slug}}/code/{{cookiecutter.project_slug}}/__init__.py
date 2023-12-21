44d4ba357c4155e26f616613c894c639fbede4d5
simon.m.hofmann <simon.hofmann@cbs.mpg.de>
2023-12-21 18:57:38 +0100

    improve import of submodules

--- research-project-comprehensive/{{cookiecutter.project_slug}}/code/{{cookiecutter.project_slug}}/__init__.py
+++ research-project-comprehensive/{{cookiecutter.project_slug}}/code/{{cookiecutter.project_slug}}/__init__.py
@@ -6,3 +6,7 @@ __version__ = "{{ cookiecutter.version }}"
 
 # %% Imports & setup
 import {{ cookiecutter.project_slug }}.configs
+import {{ cookiecutter.project_slug }}.datavisualization
+import {{ cookiecutter.project_slug }}.modeling
+import {{ cookiecutter.project_slug }}.preprocessing
+
